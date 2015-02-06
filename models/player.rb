2e21fdcd16da302371051997b356c9dc342441ef
Diego Takahashi <diego.takahashi@hotmail.com.ar>
2015-02-06 11:13:29 -0300

    Agregando las relaciones al player

--- models/player.rb
+++ models/player.rb
@@ -1,5 +1,7 @@
 require 'sinatra/activerecord'
 class Player< ActiveRecord::Base
   validates :username, presence: true, length: { maximum: 30 }
-  validates :password, presence:true, length: { minimum: 4 }
+  validates :password, presence: true, length: { minimum: 4 }
+  
+  has_many :games
 end

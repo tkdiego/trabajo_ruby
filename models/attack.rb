6d3fafb06b8bbfe2d05ebce4c6d984c993cb6e45
Agustin <agus_cipo@hotmail.com>
2015-02-08 12:23:43 -0300

    taka gil

--- models/attack.rb
+++ models/attack.rb
@@ -1,7 +1,7 @@
 require 'sinatra/activerecord'
 class Attack< ActiveRecord::Base
-  validates :id_game, presence: true
-  validates :id_player, presence: true
+  validates :game_id, presence: true
+  validates :player_id, presence: true
   validates :position, presence: true
   validates :state, presence: true
   
